5e4a3a208acbc86ea3154ddb616078bab36b31d9
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-11-27 11:57:07 +0100

    cambios

--- src/main/java/com/esliceu/PracticaDrawing2/Repos/UserRepoImpl.java
+++ src/main/java/com/esliceu/PracticaDrawing2/Repos/UserRepoImpl.java
@@ -1,6 +1,8 @@
 package com.esliceu.PracticaDrawing2.Repos;
 
 import com.esliceu.PracticaDrawing2.Entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,6 +10,29 @@ import java.util.List;
 
 @Repository
 public class UserRepoImpl implements UserRepo{
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public boolean existUser(String login) {
+        String sql = "SELECT COUNT(*) FROM user WHERE login = ?";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, login);
+        return count > 0;    }
+
+    @Override
+    public void register(User user) {
+        jdbcTemplate.update("INSERT INTO user (login,name,password) VALUES (?,?,?)",
+                user.getLogin(),user.getName(),user.getPassword());
+    }
+
+    @Override
+    public boolean login(String login, String password) {
+        String sql = "SELECT COUNT(*) FROM user WHERE login = ? AND password = ?";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, login, password);
+        return count > 0;
+    }
+
+    /*
     static List<User> userList = new ArrayList<>() ;
 
     @Override
@@ -42,4 +67,5 @@ public class UserRepoImpl implements UserRepo{
         //Si no retorna false
         return false;
     }
+     */
 }
