ab13860eab96cb2c1b1aff17c109ac66029baa78
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-11-28 11:05:46 +0100

    cambios

--- src/main/java/com/esliceu/PracticaDrawing2/Repos/UserRepoImpl.java
+++ src/main/java/com/esliceu/PracticaDrawing2/Repos/UserRepoImpl.java
@@ -31,41 +31,4 @@ public class UserRepoImpl implements UserRepo{
         int count = jdbcTemplate.queryForObject(sql, Integer.class, login, password);
         return count > 0;
     }
-
-    /*
-    static List<User> userList = new ArrayList<>() ;
-
-    @Override
-    public boolean existUser(String login) {
-        // Verifica si la lista de usuarios no es nula.
-        if (userList != null) {
-            // Recorremos la lista de usuarios.
-            for (User user : userList) {
-                // Retorna true si se encuentra un usuario con el mismo nombre de usuario.
-                if (user.getLogin().equalsIgnoreCase(login)) {
-                    return true;
-                }
-            }
-        }
-        //Si no encuentra un usuario retorna false
-        return false;
-    }
-    @Override
-    public void register(User newUser) {
-        //Añade un usuario a las lista
-        userList.add(newUser);
-    }
-    @Override
-    public boolean login(String login, String password) {
-        //Recorre la lista de usuarios
-        for (User user : userList) {
-            // Compara el nombre de usuario y la contraseña y retorna true si coinciden.
-            if (user.getLogin().equals(login) && user.getPassword().equals(password)) {
-                return true;
-            }
-        }
-        //Si no retorna false
-        return false;
-    }
-     */
 }
