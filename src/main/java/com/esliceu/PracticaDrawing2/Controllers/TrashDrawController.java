93f64afab48d33f0f7a4c3ac36883229573cb004
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-01 10:01:36 +0100

    cambios

--- src/main/java/com/esliceu/PracticaDrawing2/Controllers/TrashDrawController.java
+++ src/main/java/com/esliceu/PracticaDrawing2/Controllers/TrashDrawController.java
@@ -1,10 +1,6 @@
 package com.esliceu.PracticaDrawing2.Controllers;
 
-import com.esliceu.PracticaDrawing2.Entities.Draw;
 import com.esliceu.PracticaDrawing2.Services.DrawService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,43 +9,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
-public class DeleteDrawController {
+public class TrashDrawController {
 
     @Autowired
     HttpSession session;
 
     @Autowired
     DrawService drawService;
-/*
     @GetMapping("/DeleteDraw")
-    public String showAllDraws(Model model,@RequestParam int id) {
+    public String TrashDrawController(Model model) {
         String login = (String) session.getAttribute("login");
 
-        // Obtén todos los dibujos, incluidos los eliminados
-        List<Draw> allDraws = drawService.getAllDraws(id);
 
-        // Estos atributos se enviarán a la página JSP asociada para mostrarlos
-        model.addAttribute("allDraws", allDraws);
-        model.addAttribute("user", login);
         return "DeleteDraw"; // Nombre de tu vista que mostrará la lista de dibujos
     }
 
     @PostMapping("/DeleteDraw")
-    public String handleDeleteDraw(Model model, @RequestParam int id, @RequestParam String action) {
+    public String PostTrashDrawController(Model model, @RequestParam int id) {
         String login = (String) session.getAttribute("login");
-
-        if ("delete".equals(action)) {
-            drawService.deleteDraw(id, login);
-        }
-
-        else if ("restore".equals(action)) {
-            drawService.restoreDraw(id);
-        }
         return "redirect:/DeleteDraw";
     }
-    */
 }
 
