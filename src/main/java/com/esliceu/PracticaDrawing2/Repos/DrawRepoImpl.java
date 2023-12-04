8de909b2572aa6a0435aeb0bb40078a6394b7e57
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-04 10:10:08 +0100

    cambios

--- src/main/java/com/esliceu/PracticaDrawing2/Repos/DrawRepoImpl.java
+++ src/main/java/com/esliceu/PracticaDrawing2/Repos/DrawRepoImpl.java
@@ -50,6 +50,16 @@ public class DrawRepoImpl implements DrawRepo {
     //Metodo para mostrar las imagenes
     @Override
     public List<DrawWithVersionDTO> getDraws(int id_user) {
+        /*
+        String sql = "SELECT draw.*, MAX(version.figures) AS figures, " +
+                "MAX(version.numFigures) AS numFigures, MAX(version.modificationDate) AS modificationDate, " +
+                "permissions.permissions FROM draw " +
+                "JOIN version ON draw.id = version.id_draw " +
+                "LEFT JOIN permissions ON draw.id = permissions.id_draw AND permissions.id_user = ? " +
+                "WHERE (draw.visualization = 1 OR draw.owner_id = ? " +
+                "OR (permissions.permissions IN ('R', 'RW') AND permissions.id_user = ?)) " +
+                "AND draw.inTheTrash = 0 AND GROUP BY draw.id;";
+         */
         String sql = "SELECT draw.*, MAX(version.figures) AS figures, " +
                 "MAX(version.numFigures) AS numFigures, MAX(version.modificationDate) AS modificationDate, " +
                 "permissions.permissions FROM draw " +
@@ -60,7 +70,7 @@ public class DrawRepoImpl implements DrawRepo {
                 "AND draw.inTheTrash = 0 AND (permissions.id_user IS NULL OR permissions.id_user <> ?)) " +
                 "OR (permissions.id_user = ? AND in_your_trash = false)) GROUP BY draw.id;";
         List<DrawWithVersionDTO> allDrawWhithVersion = jdbcTemplate.query(sql,
-                new BeanPropertyRowMapper<>(DrawWithVersionDTO.class),id_user, id_user, id_user);
+                new BeanPropertyRowMapper<>(DrawWithVersionDTO.class),id_user, id_user, id_user, id_user, id_user);
         return allDrawWhithVersion;
     }
 
