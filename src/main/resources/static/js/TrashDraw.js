5eed49768db42ec2a360ab0102efd15668f5ed35
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-04 10:03:42 +0100

    cambios

--- src/main/resources/static/js/TrashDraw.js
+++ src/main/resources/static/js/TrashDraw.js
@@ -1,34 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var deleteConfirmationModal = document.getElementById('deleteConfirmationModal');
-    var confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
-    var cancelDeleteBtn = document.getElementById('cancelDeleteBtn');
+// Función para mostrar el modal
+function confirmDelete() {
+    var modal = document.getElementById('deleteModal');
+    modal.style.display = 'block';
+    return false; // Evita el envío del formulario por defecto
+}
 
-    // Manejar el clic en el botón "Eliminar" de cada fila
-    var deleteButtons = document.querySelectorAll('.delete');
-    deleteButtons.forEach(function (deleteButton) {
-        deleteButton.addEventListener('click', function (event) {
-            // Evitar el envío del formulario por defecto
-            event.preventDefault();
+// Función para cerrar el modal
+function closeModal() {
+    var modal = document.getElementById('deleteModal');
+    modal.style.display = 'none';
+}
 
-            // Mostrar el modal de confirmación
-            deleteConfirmationModal.style.display = 'block';
-
-            // Manejar el clic en el botón de confirmación del modal
-            confirmDeleteBtn.onclick = function () {
-                // Aquí podrías enviar el formulario o realizar la acción de eliminación
-                // Por ejemplo, puedes usar AJAX para enviar la solicitud al servidor
-
-                // Ocultar el modal de confirmación
-                deleteConfirmationModal.style.display = 'none';
-
-                // Ahora, puedes enviar el formulario
-                deleteButton.closest('form').submit();
-            };
-
-            // Manejar el clic en el botón de cancelar del modal
-            cancelDeleteBtn.onclick = function () {
-                // Ocultar el modal de confirmación
-                deleteConfirmationModal.style.display = 'none';
-            };
-        });
-});
+// Función para realizar la eliminación
+function deleteDraw() {
+    // Puedes agregar aquí la lógica para enviar el formulario de eliminación
+    document.getElementById('deleteForm').submit();
+}
