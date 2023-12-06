36e73d22ec290c21dea4ae3cd5101037fcca7ebb
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-06 19:31:51 +0100

    cambios

--- src/main/resources/static/js/ShareDraw.js
+++ src/main/resources/static/js/ShareDraw.js
@@ -1,8 +1,16 @@
-function actualizarCampoOculto(select) {
-   // Obtener el permiso seleccionado en el menú desplegable
-   var valorSeleccionado = select.value;
-
-   // Actualizar el valor del campo oculto con el valor seleccionado
-   document.getElementById('permission').value = valorSeleccionado;
-   console.log(valorSeleccionado);
-}
+document.addEventListener('DOMContentLoaded', function() {
+    // Se intenta obtener el elemento con el id "permisos"
+   var select = document.getElementById('permisos');
+   //Se obtiene el valor oculto.
+   var hiddenInput = document.getElementById('permission');
+   //Comprueba si no es null.
+   if (select) {
+   //Cuando cambia se actualiza el valor del input oculto.
+   select.addEventListener('change', function() {
+        hiddenInput.value = select.value;
+    });
+    } else {
+    //Si no ha encontrado.
+        console.error('El elemento con id "permisos" no fue encontrado.');
+  }
+});
