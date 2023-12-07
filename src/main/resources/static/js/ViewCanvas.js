d3352652b812babcd56d807a4680186f8f7f7221
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-07 17:33:12 +0100

    cambios

--- src/main/resources/static/js/ViewCanvas.js
+++ src/main/resources/static/js/ViewCanvas.js
@@ -1,109 +1,47 @@
-//Guardar variables
-const newCanvas = document.getElementById("ViewCanvas");
-const newCtx = newCanvas.getContext("2d");
+// app.js
+import { canvas, ctx, draw } from './draw.js';
 
-// Función para dibujar figuras
-const draw = (parsedFigures, ctx) => {
-    // Limpia el lienzo antes de dibujar las nuevas figuras
-    ctx.clearRect(0, 0, newCanvas.width, newCanvas.height);
+const drawParsedFigures = (parsedFigures) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     parsedFigures.forEach(figure => {
-        const { type, color, size, filled, coordinates } = figure;
-
-        ctx.beginPath();
-        ctx.fillStyle = color;
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 2;
-
-        const halfSize = size / 2;
-        const currentSize = filled ? size : 0;
-
-        switch (type) {
-            case "circle":
-                ctx.arc(coordinates[0].x, coordinates[0].y, halfSize, 0, 2 * Math.PI);
-                break;
-            case "square":
-                ctx.rect(coordinates[0].x - halfSize, coordinates[0].y - halfSize, size, size);
-                break;
-            case "triangle":
-                const x = coordinates[0].x;
-                const y = coordinates[0].y;
-                ctx.moveTo(x, y - halfSize);
-                ctx.lineTo(x - halfSize, y + halfSize);
-                ctx.lineTo(x + halfSize, y + halfSize);
-                ctx.lineTo(x, y - halfSize);
-                break;
-            case "star":
-                for (let i = 0; i < 14; i++) {
-                    const angle = (Math.PI * 2 * i) / 14;
-                    const radius = i % 2 === 0 ? size : size / 2;
-                    ctx.lineTo(coordinates[0].x + radius * Math.cos(angle), coordinates[0].y + radius * Math.sin(angle));
-                }
-                ctx.closePath();
-                break;
-            case "line":
-                if (coordinates && coordinates.length > 0) {
-                    ctx.moveTo(coordinates[0].x, coordinates[0].y);
-                    for (let i = 1; i < coordinates.length; i++) {
-                        ctx.lineTo(coordinates[i].x, coordinates[i].y);
-                    }
-                }
-                break;
-            default:
-                break;
-        }
-
-        if (filled) {
-            ctx.fill();
-        } else {
-            ctx.stroke();
-        }
-        ctx.closePath();
+        draw(figure);
     });
 };
 
-// Función para actualizar el canvas y el input hidden
-function updateCanvas() {
+const updateCanvas = () => {
     var selectedOption = document.querySelector('#versionSelect option:checked');
     var selectedVersionFigures = JSON.parse(selectedOption.getAttribute('data-figures'));
 
-    // Actualiza el valor del input hidden con las figuras de la versión seleccionada
     document.getElementById("selectedFigures").value = JSON.stringify(selectedVersionFigures);
-    console.log("Selected Figures: ", document.getElementById("selectedFigures").value);
 
-    // Llama a la función draw para dibujar las nuevas figuras
-    draw(selectedVersionFigures, newCtx);
-}
+    drawParsedFigures(selectedVersionFigures);
+};
 
-// Llamamos al updateCanvas al cargar la página para dibujar las figuras de la ultima version.
-updateCanvas();
-
-// Agregamos el event listener para cambiar el elemento.
 document.getElementById("versionSelect").addEventListener("change", updateCanvas);
 
-
 document.getElementById("copyButton").addEventListener("click", function() {
-   // Obténemos las figuras actualizadas del input hidden
-   const figuresDataElement = document.getElementById("selectedFigures").value;
-   const drawId = document.getElementById("drawId").value;
-   // Crea un objeto FormData y agrega las figuras
-   const formData = new FormData();
-   formData.append("jsonData", figuresDataElement);
-   formData.append("draw_Id", drawId);
-   // Realiza la solicitud Fetch para enviar los datos al servidor
-   fetch('/ViewDraw', {
+    const figuresDataElement = document.getElementById("selectedFigures").value;
+    const drawId = document.getElementById("drawId").value;
+
+    const formData = new FormData();
+    formData.append("jsonData", figuresDataElement);
+    formData.append("draw_Id", drawId);
+
+    fetch('/ViewDraw', {
         method: 'POST',
         body: formData
-   })
-   // Manejar la respuesta del servidor
-   .then(response => {
-       if (response.ok) {
+    })
+    .then(response => {
+        if (response.ok) {
             alert("¡Se ha copiado correctamente!");
-       } else {
+        } else {
             alert("Error al copiar. Tiene que tener permisos o ser el propietario");
-       }
-       })
-      .catch(error => {
-           alert("Error de red. Por favor, inténtalo de nuevo más tarde.");
-      });
+        }
+    })
+    .catch(error => {
+        alert("Error de red. Por favor, inténtalo de nuevo más tarde.");
+    });
 });
+
+updateCanvas();
