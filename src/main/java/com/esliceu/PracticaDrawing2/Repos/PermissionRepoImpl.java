81332f6ba155a37db6d74c6d5f5f07be9e2085a1
Andres Carrasco Dols <acarrascod@esliceu.net>
2023-12-08 21:57:22 +0100

    cambios

--- src/main/java/com/esliceu/PracticaDrawing2/Repos/PermissionRepoImpl.java
+++ src/main/java/com/esliceu/PracticaDrawing2/Repos/PermissionRepoImpl.java
@@ -16,24 +16,28 @@ public class PermissionRepoImpl implements PermissionRepo{
         jdbcTemplate.update("INSERT INTO permissions (id_draw,id_user,permissions) VALUES (?,?,?)",
                 permissions.getId_draw(),permissions.getId_users(),permissions.getPermissions());
     }
+
     //Metodo para actualizar permisos.
     @Override
     public void updatePermission(Permissions permissions) {
         String updateSql = "UPDATE permissions SET permissions = ? WHERE id_draw = ? AND id_user = ?";
         jdbcTemplate.update(updateSql, permissions.getPermissions(), permissions.getId_draw(), permissions.getId_users());
     }
+
     //Metod para comprobar si tiene permisos
     @Override
     public boolean ExistpermissionUser(int drawId, int userId) {
         String selectSql = "SELECT COUNT(*) FROM permissions WHERE id_draw = ? AND id_user = ?";
         int count = jdbcTemplate.queryForObject(selectSql, Integer.class, drawId, userId);
         return count > 0;    }
+
     //Metodo para borrar permisos
     @Override
     public void deletePermissionsUser(int drawId, int userId) {
         String deleteSql = "DELETE FROM permissions WHERE id_draw = ? AND id_user = ?";
         jdbcTemplate.update(deleteSql, drawId, userId);
     }
+    
     //Metodo para actalizar papelera
     @Override
     public void updatePermissionTrash(int id) {
